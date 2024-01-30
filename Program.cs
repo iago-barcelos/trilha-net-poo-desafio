6bb4f27e8f8592393b3ad213a7379defbe8339ef
Iago Barcelos <iago.cmpb@gmail.com>
2024-01-30 17:56:53 -0300

    projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: "1234", modelo: "model1", imei: "4321", memoria: 10);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("tigrinho");
+
+
+Smartphone iphone = new Iphone(numero: "5678", modelo: "model2", imei: "8765", memoria: 10);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("YouTube");
+iphone.ReceberLigacao();
